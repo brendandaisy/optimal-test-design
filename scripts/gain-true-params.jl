1a52109fefe8818cb0c0d136282a0b17abc109d0
brendandaisy <bcase@uvm.edu>
2022-12-13 14:11:15 -0500

    random cleanup. Planning on making new repo from this

--- scripts/gain-true-params.jl
+++ scripts/gain-true-params.jl
@@ -1,6 +1,9 @@
+## gain-trueparams.jl
+## run the experiment shown in Figure 2, where PI up to peak is assessed for varying
+## θtrue of different outbreak intensities
+
 using DrWatson
 @quickactivate "optimal-test-design"
-using Revise
 using DiffEqInformationTheory
 using ConditionalTransform
 using Distributions, MonteCarloMeasurements
@@ -18,6 +21,11 @@ function sir_from_samples(α, β, S₀)
     )
 end
 
+"""
+For the given `θtrue`, run the SIR simiulations while holding u = f(`θtrue`) fixed for each u
+
+Returns a `Dict` of infection curves for each u
+"""
 function solve_inf_cond(θtrue, θprior, lat_mod)
     silent=true
     nsamples=nparticles(lat_mod.β)
@@ -52,7 +60,7 @@ function solve_inf_cond(θtrue, θprior, lat_mod)
             θprior, imax, inv_max_inf, d_inv_max_inf; 
             pivot=:S₀, nsamples, silent=true
         )
-    else
+    else # all parameter combinations causing no outbreak have equal weight
         αcond, βcond, Scond = sample_trunc_f(θprior, (a, b, S)->reff(a, b, S) > 1; nsamples)
     end
     sir_cond = sir_from_samples(αcond, βcond, Scond)
@@ -70,7 +78,14 @@ function solve_inf_cond(θtrue, θprior, lat_mod)
     return inf_conds
 end
 
+"""
+Test practical identifiability of each variable over a set of `θtrue_set` dynamics. PI is calculated using observations
+up to the first day after the true disease peak
+
+Returns a `Vector{NamedTuple}` containing results for each θtrue
+"""
 function ident_over_θtrue(θtrue_set, θprior, obs_mod; N=100, M=60_000)
+    # simulate from the full prior
     lat_mod = SIRModel{Float32}(
         S₀=Particles(M, θprior.S₀), 
         β=Particles(M, θprior.β), 
@@ -81,6 +96,7 @@ function ident_over_θtrue(θtrue_set, θprior, obs_mod; N=100, M=60_000)
     ret = []
     for θtrue in θtrue_set
         @show θtrue
+        # solve true SIR system, dense=true to allow finding peak timing
         sol_true = solve(lat_mod, θtrue; save_idxs=2, dense=true)
         f = t -> -sol_true(t)
         peak_t = ceil(Int, optimize(f, 0, 30).minimizer)
@@ -100,11 +116,7 @@ function ident_over_θtrue(θtrue_set, θprior, obs_mod; N=100, M=60_000)
 end
 
 θprior = (α=Uniform(0.05f0, 0.85f0), β=Uniform(0.3f0, 1.5f0), S₀=Uniform(0.1f0, 0.99f0))
-# θtrue_set = [(α=0.2, β=b, S₀=S) for b in Float32.(0.3:0.2:1.5) for S in Float32.(0.1:0.2:0.9)]
-θtrue_set = [(α=0.2f0, β=0.7f0, S₀=0.3f0), (α=0.2f0, β=0.3f0, S₀=0.9f0), (α=0.2f0, β=0.5f0, S₀=0.5f0)]
-# tmp = solve(SIRModel{Float32}(α=0.2f0, β=0.3, S₀=0.7), save_idxs=2, dense=true)
-# plot(tmp)
-# θtrue_set = filter(x->reff(x...) > 0.9, θtrue_set)
+θtrue_set = [(α=0.2, β=b, S₀=S) for b in Float32.(0.3:0.2:1.5) for S in Float32.(0.1:0.2:0.9)]
 obs_mod = PoissonTests(1000)
 
 res = ident_over_θtrue(θtrue_set, θprior, obs_mod; N=3000, M=60_000)
